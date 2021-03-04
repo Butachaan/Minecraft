ffc3e68021c24ff6c7c9e6098f2e6cf72718ac61
Butachaan <butaokara1202@gmail.com>
2021-03-04 23:01:46 +0900

    as

--- bot.py
+++ bot.py
@@ -22,4 +22,18 @@ async def _slash_hello(ctx: SlashContext):
 async def on_ready():
   print('bot ready.')
 
+@bot.command()
+async def config(ctx, config_name, config_arg):
+    if config_name == 'langage':
+        if config_arg == 'ja':
+            set_langage = 'ja'
+            config_output = '言語を日本語に設定しました'
+        elif config_arg == 'en':
+            set_langage = 'en'
+            config_output = '言語を英語に設定しました'
+        else:
+            config_output = '構文エラー。jpまたはenを指定してください。'
+
+    await ctx.send(config_output)
+
 bot.run(token)
